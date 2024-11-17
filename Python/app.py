63ed5ed833319d9b8d47bb437ca61b03c83e513a
Roshan <chyroshan066@gmail.com>
2024-11-17 11:43:41 +0545

    Learnt how to write multiline strings in python

--- Python/app.py
+++ Python/app.py
@@ -1,18 +1,5 @@
 # print("Hello World")
 
-# **************************************************Strings*******************************************************
-# MULTILINE STRINGS
-'''multilineString = ("Learning the ins and outs of advanced Jira functionalities, from detailed project tracking to customized workflows, "
-                   "has given me a powerful toolkit for effective project management and collaboration. I’m thrilled to bring these skills"
-                   " into my future work, ensuring projects run smoothly and teams stay aligned.")'''
-# multilineString = '''Learning the ins and outs of advanced Jira functionalities, from detailed project tracking to customized workflows,
-# has given me a powerful toolkit for effective project management and collaboration. I’m thrilled to bring these skills into my future work,
-# ensuring projects run smoothly and teams stay aligned.'''
-'''multilineString = """Learning the ins and outs of advanced Jira functionalities, from detailed project tracking to customized workflows, 
-has given me a powerful toolkit for effective project management and collaboration. I’m thrilled to bring these skills into my future work, 
-ensuring projects run smoothly and teams stay aligned."""'''
-# print(multilineString)
-
 # STRING METHODS
 course = "python for Beginners"
 # print(course.upper())  # Converts string to uppercase
---
+++ Python/multiline_string.py
@@ -0,0 +1,11 @@
+'''multilineString = ("Learning the ins and outs of advanced Jira functionalities, from detailed project tracking to customized workflows, "
+                   "has given me a powerful toolkit for effective project management and collaboration. I’m thrilled to bring these skills"
+                   " into my future work, ensuring projects run smoothly and teams stay aligned.")'''
+# multilineString = '''Learning the ins and outs of advanced Jira functionalities, from detailed project tracking to customized workflows,
+# has given me a powerful toolkit for effective project management and collaboration. I’m thrilled to bring these skills into my future work,
+# ensuring projects run smoothly and teams stay aligned.'''
+multilineString = """Learning the ins and outs of advanced Jira functionalities, from detailed project tracking to customized workflows, 
+has given me a powerful toolkit for effective project management and collaboration. I’m thrilled to bring these skills into my future work, 
+ensuring projects run smoothly and teams stay aligned."""
+
+print(multilineString)
