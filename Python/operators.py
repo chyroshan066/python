163d64e5a777fe48445b971850a61a6ddd97d8c9
Roshan <chyroshan066@gmail.com>
2024-11-17 20:00:52 +0545

    Learnt operators

---
+++ Python/operators.py
@@ -0,0 +1,38 @@
+# ARITHMETIC OPERATORS
+'''print(10 / 3)
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 // 3)  # Removes decimal part
+print(10 ** 3)  # Exponentiation
+print(10 % 3)'''
+
+# OPERATOR PRECEDENCE
+'''print(10 + 2 * 3)
+print((10 + 2) * 3)'''
+
+# COMPARISON OPERATORS
+'''print(3 > 2)
+print(3 >= 2)
+print(3 < 2)
+print(3 <= 2)
+print(3 == 2)
+print(3 != 2)'''
+
+# LOGICAL OPERATORS
+'''price = 25
+print(price>10 and price>30)
+print(price>10 or price<30)
+print(not price>10)'''
+
+# IDENTITY OPERATORS
+'''x = 10
+y = 10
+print(x is y)
+a = b = 20
+print(a is b)
+print(id(a))
+print(id(b))'''
+list1= [10, 20, 30]
+list2= [10, 20, 30]
+print(list1 is list2)  # Python doesn't assign the same memory location to the collection of values
