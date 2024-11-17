cec74dcc1ae4449ffde9e52c12d6ec5833597d37
Roshan <chyroshan066@gmail.com>
2024-11-17 20:31:34 +0545

    Learnt if else statement

---
+++ Python/if_statement.py
@@ -0,0 +1,12 @@
+temperature = 15
+'''if temperature > 30:
+    print("It's a hot day")
+elif temperature > 20:
+    print("It's a nice day")
+else:
+    print("It's bit cold")'''
+
+# SHORTHAND IF ELSE
+# print("It's a hot day") if temperature > 30 else print("It's a nice day") if temperature > 20 else print("It's bit cold")
+c = 9 if 10>20 else 0
+print(c)
