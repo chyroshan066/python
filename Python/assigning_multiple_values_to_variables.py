b49e8e8165d8575b33f8175d7f1654090ac0c8c1
Roshan <chyroshan066@gmail.com>
2024-11-17 11:38:12 +0545

    Learnt how to assign multiple values to multiple variables at a time

---
+++ Python/assigning_multiple_values_to_variables.py
@@ -0,0 +1,21 @@
+# Assigning multiple values to multiple variables at a time
+'''x, y, z = "Apple", "Banana", "Orange"
+print(x)
+print(y)
+print(z)'''
+
+# Assigning same values to multiple variables at a time
+'''x = y = z = "Apple"
+print(x)
+print(y)
+print(z)'''
+
+# Unpacking a collection
+fruits = ["Apple", "Banana", "Orange"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+print(x, z)
+'''a = 10
+print(x + a)  # Throws an error'''
