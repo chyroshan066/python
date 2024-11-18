d18835aff8e6cd843cc64fa9f458813bbac0882f
Roshan <chyroshan066@gmail.com>
2024-11-18 23:42:54 +0545

    Understood range function

---
+++ Python/range.py
@@ -0,0 +1,8 @@
+'''for x in range(10):
+    print(x)'''
+
+'''for x in range(10, 15):  # When two values are specified, then first value is the starting value and the second value is range
+    print(x)'''
+
+for x in range(10, 15, 2):  # When three values are specified, then first value is the starting value, the second value is range, and the third value is step
+    print(x)
