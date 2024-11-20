77b36ddad2f1da25aaf625fcf1c6a4499c72b9a8
Roshan <chyroshan066@gmail.com>
2024-11-20 21:06:34 +0545

    Learnt various set methods

---
+++ Python/set_methods.py
@@ -0,0 +1,35 @@
+s1 = {1, 3, 5, 8}
+s2 = {3, 5}
+
+# print(s1.union(s2))  # Returns the union of two set but doesn't update any
+
+# s1.update(s2)  # Adds the elements of iterables to the set
+
+# print(s1.intersection(s2))  # Returns the intersection of two set but doesn't update any
+
+# s1.intersection_update(s2)  # Updates the intersection of two set but doesn't return any value
+
+# print(s1.symmetric_difference(s2))  # Returns the set of items that are not common  in two sets but doesn't update the first set
+
+# print(s1.difference(s2))  # Returns the set of items that are present in first set but not in second set. It  doesn't update the first set
+
+# print(s1.isdisjoint(s2))  # Checks whether the two sets have common intersection or not
+
+# print(s1.issuperset(s2))  # Checks whether the first set contains the element of the second set or not
+
+# print(s2.issubset(s1))  # Checks whether the elements of first set is in the second set or not
+
+# s1.add(11)  # Adds a single element to the set
+
+# s1.remove(5)  # Removes the element from the set
+# s1.remove(2)  # Raise an error if the specified element is not present
+
+# s1.discard(2)  # Does not raise an error if the specified element is not present
+
+# print(s1.pop())  # Removes random element from the set
+
+# del s1  # Deletes the set
+
+print(s1)
+
+# print(s1, s2)
---
+++ Python/sets.py
@@ -0,0 +1,8 @@
+s = {"Roshan", 1, True, 3}
+
+# CREATING AN EMPTY SET
+# empty_set = {}  # This will not be an empty set but instead be an empty dictionary
+empty_set = set()  # This is the correct way of creating set using set() constructor
+print(type(empty_set))
+
+# print(s)
