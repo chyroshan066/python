34701a3c4b2da613812e2d607db12d7d5f8e93bd
Roshan <chyroshan066@gmail.com>
2024-11-24 01:34:39 +0545

    Learnt JSON in python

--- Python/JSON.py
+++ Python/JSON.py
@@ -1,14 +1,18 @@
 import json
 import requests
+from pprint import pprint
 
 dictionary = {
     "course_name": "Python",
     "fee": 15000
 }
-response = requests.get("https://www.codewithharry.com")
-res_text = response.json
-print(res_text)
-
 json_format = json.dumps(dictionary)  # Conversion from python object to JSON
-
 # print(type(json_format))
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
+dict_format = json.loads(response.text)  # Converts JSON object to dict
+# pprint(dict_format["title"])
+for item in dict_format:
+    if item["id"] == 5:
+        print(item["title"])
+
